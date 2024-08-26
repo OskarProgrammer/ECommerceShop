c3c8aa1a64e2629be3e725d7fdeee80e04b4e195
Oskar <>
2024-08-26 14:33:53 +0200

    Adding conditional rendering for form of adding to the basket

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { getRequestID } from "../API/getRequestID"
 import { putRequest } from "../API/putRequest"
 
 export const HomePage = () => {
-    const items = useLoaderData()
+    const [items, currentUser] = useLoaderData()
 
     return (
         <div className="container text-center">
@@ -19,11 +19,11 @@ export const HomePage = () => {
                         <p>{item.price}</p>
                         <h5>Amount:</h5>
                         <p>{item.amount}</p>
-                            <Form method="post" action="/" className="d-flex flex-column p-3">
+                            {currentUser.isLogged ? <Form method="post" action="/" className="d-flex flex-column p-3">
                                 <h5>Id of product: <span name="id">{item.id}</span></h5>
-                            <input type="number" name="amount" className="col-lg-2 col-5 mx-auto mb-2 text-center" />
-                            <button type="submit" className="btn btn-outline-success btn-lg col-lg-5 mx-auto ">Add to basket</button>
-                        </Form>
+                                <input type="number" name="amount" className="col-lg-2 col-5 mx-auto mb-2 text-center" />
+                                <button type="submit" className="btn btn-outline-success btn-lg col-lg-5 mx-auto ">Add to basket</button>
+                            </Form> : ""}
                     </div>}
                     {index % 2 == 0 ? <div className="col-lg-6 text-light p-3">
                         <h5 className="">Description:</h5>
@@ -32,11 +32,11 @@ export const HomePage = () => {
                         <p>{item.price}</p>
                         <h5>Amount:</h5>
                         <p>{item.amount}</p>
-                        <Form method="post" action="/" className="d-flex flex-column p-3">
+                        {currentUser.isLogged ? <Form method="post" action="/" className="d-flex flex-column p-3">
                             <h5>Id of product: <span name="id">{item.id}</span></h5>
                             <input type="number" name="amount" className="col-lg-2 col-5 mx-auto mb-2 text-center" />
                             <button type="submit" className="btn btn-outline-success btn-lg col-lg-5 mx-auto ">Add to basket</button>
-                        </Form>
+                        </Form> : ""}
                     </div> : <div className="col-lg-6 my-auto text-light">
                         <h2 className="display-3 bg-light text-dark rounded-pill">{item.name}</h2>
                     </div>}
@@ -48,8 +48,9 @@ export const HomePage = () => {
 
 export const homePageLoader = async () => {
     const items = await getRequest("http://localhost:3000/items/")
+    const currentUser = await getRequest("http://localhost:3000/currentUser/")
 
-    return items
+    return [items, currentUser]
 }
 
 export const homePageAction = async ({request}) => {
