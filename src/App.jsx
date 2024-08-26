7f166d845ef67c1657095e0abe51b10a9ff130c2
Oskar <>
2024-08-26 11:46:44 +0200

    Creating router instation, creating paths: '/' and '/login'

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+//import bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+
 import './App.css'
 
+//import react router dom components
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+//importring layouts
+import { HomeLayout } from './layouts/HomeLayout'
+
+//importing pages
+import { ErrorPage } from './pages/ErrorPage'
+import { HomePage } from './pages/HomePage'
+import { LoginPage } from './pages/LoginPage'
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout/>} errorElement={<ErrorPage/>}> 
+      <Route index element={<HomePage/>}/>
+      <Route path="/login" element={<LoginPage />} />
+    </Route>
+  )
+)
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router}/>
     </>
   )
 }
